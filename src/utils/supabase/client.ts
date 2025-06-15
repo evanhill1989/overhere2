b8f6dca92ed8bb243e35d1c2662fdb474865e402
Evan Hill <evanhill1989@gmail.com>
2025-06-15 11:56:45 -0400

    pkce flow

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,5 +3,10 @@ export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        flowType: "pkce", // 👈 This is critical
+      },
+    },
   );
 }
