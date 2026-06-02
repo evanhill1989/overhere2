04fae13c527896d768eb4e0bbc8d753ece7d4be5
EvanReact <evanhill1989@gmail.com>
2026-06-02 15:33:47 -0400

    logofix header

--- src/components/LogoWithWave.tsx
+++ src/components/LogoWithWave.tsx
@@ -9,7 +9,7 @@ export default function LogoWithWave() {
     <Link href="/">
       <div className="group flex items-center gap-2">
         <h1 className="font-heading active:underline active:underline-offset-2">
-          overhere
+          OverHere
         </h1>
         <div className="icon-wrapper group-hover:animate-wave-hover origin-[appropriate-for-wrapper]">
           <HandWaving className="animate-wave-load h-5 w-5 origin-[appropriate-for-icon]" />
